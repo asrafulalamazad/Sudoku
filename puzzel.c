e9f824de297aa59278f3e88956b6c8d840c3a7dd
frog <frog@frog>
2019-03-23 15:40:56 -0400

    printing sudoku proble

---
+++ puzzel.c
@@ -0,0 +1,50 @@
+#include "sudoku.h"
+
+int ** createPuzzle(){
+    int ** puzzle;
+    int i,j;
+    int array[9][9] = {3, 4, 0,   0, 0, 0,   0, 7, 0,
+                       8, 0, 0,   4, 0, 7,   2, 5, 0,
+                       7, 0, 6,   8, 0, 0,   3, 0, 9,
+
+                       0, 1, 3,   0, 0, 6,   4, 0, 0,
+                       0, 0, 7,   0, 0, 4,   0, 1, 0,
+                       0, 0, 4,   0, 0, 0,   6, 0, 3,
+                       
+                       0, 7, 9,   6, 5, 0,   1, 0, 2,
+                       0, 0, 0,   7, 0, 0,   5, 9, 8,
+                       0, 3, 0,   2, 9, 1,   7, 0, 0,};
+        
+        puzzle = (int**)malloc(sizeof(int*)*9);
+        for(size_t i = 0; i < 9; i++){
+            puzzle[i] =(int*)malloc(sizeof(int)*9);
+            for(size_t j = 0; j < 9; j++){
+                puzzle[i][j] = array[i][j];
+            }
+            
+        }
+        return puzzle;
+}
+
+// print data infor the terminal
+void printPuzzle(int ** puzzle){
+    printf("+-----------------------------+\n");
+    for(size_t i = 0; i < 9; i++){
+        /* code */
+        printf("|");
+        for(size_t j = 0; j < 9; j++){
+            /* code */
+            printf(" %d ",puzzle[i][j]);
+            if(((j +1) %3) == 0){
+                printf("|");
+            }
+        }
+        printf("\n");
+        if(((i+1) % 3) ==0){
+            printf("+-----------------------------+\n");
+        }
+        
+    }
+
+    
+}
