8c19ec9e89e1a07197fed82d552aa0aff4e69e05
frog <frog@frog>
2019-03-23 16:27:24 -0400

    adding setUpPuzzle function in puzzle.c file and addinf data type in the sudoku.h file

--- puzzel.c
+++ puzzel.c
@@ -1,8 +1,28 @@
 #include "sudoku.h"
 
+Square *** setUpPuzzle(int ** puzzle){
+    Square *** sudoku;
+    sudoku = (Square***)malloc(sizeof(Square**)*9);
+    for(size_t i = 0; i < 9; i++){
+        /* code */
+        sudoku[i] =(Square***)malloc(sizeof(Square**)*9);
+        for(size_t j = 0; j < 9; j++){
+            /* code */
+            sudoku[i][j] = (Square*)malloc(sizeof(Square)*9);
+            sudoku[i][j]->number = puzzle[i][j];
+            sudoku[i][j]->row = i;
+            sudoku[i][j]->column = j;
+            if(sudoku[i][j]->number != 0)
+                sudoku[i][j]->code = POSIBLE;
+            else
+                sudoku[i][j]->code = 0x0;
+        }   
+    }
+}
+
 int ** createPuzzle(){
     int ** puzzle;
-    int i,j;
+    // int i,j;
     int array[9][9] = {3, 4, 0,   0, 0, 0,   0, 7, 0,
                        8, 0, 0,   4, 0, 7,   2, 5, 0,
                        7, 0, 6,   8, 0, 0,   3, 0, 9,
@@ -43,8 +63,5 @@ void printPuzzle(int ** puzzle){
         if(((i+1) % 3) ==0){
             printf("+-----------------------------+\n");
         }
-        
-    }
-
-    
+    }   
 }
--- sudoku.c
+++ sudoku.c
@@ -2,9 +2,11 @@
 
 int main(){
     int ** puzzle;
+    Square *** sudoku;
+
     puzzle = createPuzzle();
-    
     printPuzzle(puzzle);
+    sudoku = setUpPuzzle(puzzle);
 
     return 0;
 
